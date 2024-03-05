4772baf9c8ea4a26930ca01567ab1169183ff2d1
Avinash01 <avinash.m1302@gmail.com>
2024-03-05 19:06:56 +0530

    done

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function generateRandomGradient() {
+  const startColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  const endColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  const gradientDirection = `${Math.floor(Math.random() * 360)}deg`;
+  const gradientCSS = `background: linear-gradient(${gradientDirection}, ${startColor}, ${endColor})`;
+  document.getElementById('gradient-box').style.cssText = gradientCSS;
+}
+
+generateRandomGradient();
+
+function updateTime() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let ampm = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+  document.getElementById("ampm").textContent = ampm;
+}
+
+setInterval(updateTime,1000);
+updateTime();
